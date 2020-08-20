c527d50d7b7671fc2c228515d5865a9202953045
HoangLong-Anoko <54540022+HoangLong-Anoko@users.noreply.github.com>
2020-08-20 18:11:15 +0700

    student binding

---
+++ StudentBinding/StudentBinding/ViewModels/MainPageViewModels.cs
@@ -0,0 +1,37 @@
+﻿using StudentBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentBinding.ViewModels
+{
+    class MainPageViewModels
+    {
+        public ObservableCollection<Models.Student> ListStudents { get; private set; }
+            = new ObservableCollection<Models.Student>();
+        public MainPageViewModels()
+        {
+            ListStudents.Add(new Models.Student()
+            {
+                Id = "AA001",
+                Name = "Hoang Long",
+                Gpa = 8.5f,
+            });
+            ListStudents.Add(new Models.Student()
+            {
+                Id = "AA002",
+                Name = "Hoang Anh",
+                Gpa = 8.0f,
+            });
+            ListStudents.Add(new Models.Student()
+            {
+                Id = "AA003",
+                Name = "Hoang Lam",
+                Gpa = 7.0f,
+            });
+        }
+    }
+}
