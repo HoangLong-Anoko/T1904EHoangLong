cc8809547b56adb2e4eae548ed67a4f77f52d7c5
HoangLong-Anoko <54540022+HoangLong-Anoko@users.noreply.github.com>
2020-09-26 15:30:59 +0700

    practice

---
+++ PRACTICE/Services/JsonFileProductService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using ProductManager.Models;
+
+namespace ProductManager.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        //public void AddRating(string productId, int rating)
+        //{
+        //    var products = GetProducts();
+        //    if (products.First(x => x.Id == productId).Ratings == null)
+        //    {
+        //        products.First(x => x.Id == productId).Ratings = new int[] { rating };
+        //    }
+        //    else
+        //    {
+        //        var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+        //        ratings.Add(rating);
+        //        products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+        //    }
+
+        //    using (var outputStream = File.OpenWrite(JsonFileName))
+        //    {
+        //        JsonSerializer.Serialize<IEnumerable<Product>>(
+        //            new Utf8JsonWriter(outputStream, new JsonWriterOptions
+        //            {
+        //                SkipValidation = true,
+        //                Indented = true
+        //            }),
+        //            products
+        //        );
+        //    }
+        //}
+    }
+}
---
+++ Workshop2345/Workshop2345/ws3webform.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ws3webform.aspx.cs" Inherits="Workshop2345.ws3webform" %>
+<%@ Register Src="~/UserControll/LeftMessage.ascx" TagPrefix="uc1" TagName="LeftMessage" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        <uc1:LeftMessage runat="server" ID="LeftMessage"/>
+    </div>
+        <h3> Welcome! </h3>
+        <div>
+        </div>
+        </form>
+</body>
+</html>
