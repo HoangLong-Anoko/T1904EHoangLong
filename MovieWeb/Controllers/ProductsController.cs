a2c3230099e8355dc8a2ef56fd35d4bec949a370
HoangLong-Anoko <54540022+HoangLong-Anoko@users.noreply.github.com>
2020-10-01 17:20:30 +0700

    s

---
+++ MovieWeb/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieWeb.Models;
+using MovieWeb.Services;
+
+namespace MovieWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+
+        //[HttpPatch]
+        //public ActionResult Patch([FromBody] RatingRequest request)
+        //{
+        //    ProductService.AddRating(request.ProductId, request.Rating);
+
+        //    return Ok();
+        //}
+
+        //public class RatingRequest
+        //{
+        //    public string ProductId { get; set; }
+        //    public int Rating { get; set; }
+        //}
+    }
+}
